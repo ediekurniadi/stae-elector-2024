9ecf3d4a7cfb21631e933182b36b61d3d710b77e
ediekurniadi <ideedi.programmer@gmail.com>
2023-10-20 22:43:15 +0700

    Seting koneksi database dan error handle

---
+++ server/utils/db.ts
@@ -0,0 +1,22 @@
+require("dotenv").config();
+import oracledb, { OUT_FORMAT_OBJECT, OUT_FORMAT_ARRAY } from "oracledb";
+
+// database oracle
+const dbConfig = {
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  connectString: process.env.DB_STRING,
+};
+
+async function connectToDatabase() {
+  try {
+    const connection = await oracledb.getConnection(dbConfig);
+    oracledb.outformat = OUT_FORMAT_OBJECT;
+    return connection;
+  } catch (error) {
+    console.log("Error connecting to the Oracle database", error);
+    process.exit(1);
+  }
+}
+
+module.exports = { connectToDatabase };
